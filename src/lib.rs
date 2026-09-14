1dc34b8a6e8eef09011a77cf812994a77346d0dd
Bill Figliolini <46942174+Bill-Figliolini@users.noreply.github.com>
2026-09-14 18:30:08 -0400

    Moved compile error to lib so it effects library use, added clock module

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,14 @@ use std::{path::Path, process::abort};
 use crate::{bitmanip::Bitfield, cpu::Cpu, memory::rom::Rom};
 
 mod bitmanip;
+mod clock;
 mod cpu;
 mod helpers;
 mod instructions;
 mod memory;
 
+#[cfg(not(any(target_pointer_width = "32", target_pointer_width = "64")))]
+compile_error!("Only 32-bit and 64-bit architectures supported");
 pub fn nyx_main() {
     let mut cpu = Cpu::new();
     let instruction = memory::rom::parse(Bitfield::new(0));
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
 fn main() {
-    #[cfg(not(any(target_pointer_width = "32", target_pointer_width = "64")))]
-    compile_error!("Only 32-bit and 64-bit desktop architectures supported");
     nyxgba::nyx_main();
 }
