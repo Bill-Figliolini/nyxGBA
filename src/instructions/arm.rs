87ca9e31a419349b8c3860b34dfa28ccb1fbf9a4
Bill Figliolini <46942174+Bill-Figliolini@users.noreply.github.com>
2026-09-23 18:38:26 -0400

    Testing ArmCondition

--- src/instructions/arm.rs
+++ src/instructions/arm.rs
@@ -10,9 +10,9 @@ pub(crate) struct ArmCommand {
     pub read_reg: Register,
     pub source: SourceOperand,
 }
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 #[repr(u8)]
-#[expect(dead_code, reason = "To be used Later")]
+#[cfg_attr(not(test), expect(dead_code, reason = "To be used later"))]
 pub(crate) enum ArmCondition {
     Equal,
     NotEqual,
@@ -64,4 +64,44 @@ pub(crate) enum ArmOpCode {
 pub(crate) enum SourceOperand {
     Immediate(u32),
     Register(Register),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    mod condition {
+        use super::*;
+        #[test]
+        fn accepts_values_from_0_to_f() {
+            let results = vec![
+                ArmCondition::Equal,
+                ArmCondition::NotEqual,
+                ArmCondition::CarrySet,
+                ArmCondition::CarryCleared,
+                ArmCondition::Minus,
+                ArmCondition::Plus,
+                ArmCondition::SignedOverflow,
+                ArmCondition::NoSignedOverflow,
+                ArmCondition::UnsignedHigher,
+                ArmCondition::UnsignedLowerOrSame,
+                ArmCondition::SignedGreaterEq,
+                ArmCondition::SignedLesser,
+                ArmCondition::SignedGreater,
+                ArmCondition::SignedLesserEq,
+                ArmCondition::Always,
+                ArmCondition::Never,
+            ];
+            let mut results_iter = results.into_iter();
+            for i in 0..0x10 {
+                assert_eq!(ArmCondition::new(i), results_iter.next().unwrap());
+            }
+            assert_eq!(results_iter.next(), None);
+        }
+
+        #[test]
+        #[should_panic = "assertion failed: input < 0x10"]
+        fn debug_panics_on_value_outside_range() {
+            ArmCondition::new(0x10);
+        }
+    }
 }
